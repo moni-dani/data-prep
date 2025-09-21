abe712cc208fdb1f35af3aac134dd7da74d8e5c1
Monica Espinosa <monicadan26@gmail.com>
2025-09-21 01:44:28 -0600

    feat: implementa el análisis de percepción de inseguridad para todos los trimestres

--- ver_resultados.py
+++ ver_resultados.py
@@ -1,11 +1,9 @@
 import pandas as pd
 
-# Define la ruta al archivo de resultados que quieres ver.
-# Puedes cambiar este nombre de archivo para ver datos de otro trimestre.
+
 file_path = "processed_data/yucatan_insecurity_conjunto_de_datos_ensu_cb_0625.csv"
 
-# Lee el archivo CSV en un DataFrame de pandas
 df_resultados = pd.read_csv(file_path)
 
-# Imprime el DataFrame para mostrarlo como una tabla en la terminal
+# DataFrame 
 print(df_resultados)
